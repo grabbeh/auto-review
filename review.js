f41f129a3a324840ed085f8b8bc82903797103de
Michael <mbg@outlook.com>
2020-04-24 13:27:35 +0100

    Create review.js

---
+++ review.js
@@ -0,0 +1,10 @@
+
+[{
+"keywords":["Force majeure", "act of god", "relieved"],
+"agreementCategory":"general",
+"meaning":"Agreements often incorporate some language which likely relieves a party (or parties) of obligations in the event of an unforeseen event",
+"approach":["This type of language is fairly common as it's unreasonable to expect parties to comply with their obligations where they're affected by unforeseen issues."],
+"clientTip":["Check to see if the clause is appropriately scoped in terms of the events which would give rise to a release of obligations","You should ensure the supplier at least uses reasonable endeavours to recommence performance within a reasonable time", "You probably want a termination right if the supplier fails to recommence performance within a reasonable number of days"],
+"supplierTip":"You might want to think about ensuring that payment obligations are carved out of the release from obligations by the client",
+"noReference":"This agreement seems to lack a force majeure clause. If you're the customer, this may be a good thing because the supplier can't get out performing its obligations. If you're the supplier, you should think about including one.]
+}]
